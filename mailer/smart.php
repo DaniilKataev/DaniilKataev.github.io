3f43de1c425e98783017fecb26bd6d269d98ae72
kamaz777 <kataev.danechka@mail.ru>
2023-05-07 13:45:43 +0300

    Новая версия

--- mailer/smart.php
+++ mailer/smart.php
@@ -1,44 +1,51 @@
 <?php 
-echo var_dump($_POST);
-$name = $_POST['name'];
-$email = $_POST['email'];
-$text = $_POST['text'];
-
+echo var_dump($_POST);      //Получаем POST запрос
+$name = $_POST['name'];     //имя пользователя
+$email = $_POST['email'];   //почта
+$text = $_POST['text'];     //сообщение пользовтеля
+//подключаем файл и создаем объект сообщения
 require_once('phpmailer/PHPMailerAutoload.php');
 $mail = new PHPMailer;
+//Кодировка сообщения
 $mail->CharSet = 'utf-8';
 
-// $mail->SMTPDebug = 3;                               // Enable verbose debug output
-
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.mail.ru';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'kataev.danechka@mail.ru';                 // Наш логин
-$mail->Password = 'CWjdJ30zhYkt6SvgXay7';                           // Наш пароль от ящика
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465;                                    // TCP port to connect to
+$mail->isSMTP();                                      // использование техн. SMTP
+$mail->Host = 'smtp.mail.ru';                         // url почты
+$mail->SMTPAuth = true;                               // авторизация
+$mail->Username = 'kataevwebdev@mail.ru';             // наш логин
+$mail->Password = 'reJmXL8DYi2mQ8LcyALN';             // пароль для внешних ресурсов
+$mail->SMTPSecure = 'ssl';                            // тип подключения
+$mail->Port = 465;                                    // порт сервера
  
-$mail->setFrom('kataev.danechka@mail.ru', 'Portfolio');   // От кого письмо 
-$mail->addAddress($email);     // Add a recipient
-//$mail->addAddress('ellen@example.com');               // Name is optional
-//$mail->addReplyTo('info@example.com', 'Information');
-//$mail->addCC('cc@example.com');
-//$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+$mail->setFrom('kataevwebdev@mail.ru', 'Web-разработчик Даниил Катаев');   // От кого письмо 
+$mail->addAddress('dkataev1@vk.com');                 // получаетль
+$mail->isHTML(true);                                  // тип сообщения - html
 
-$mail->Subject = 'Данные';
-$mail->Body    = '
+$mail->Subject = 'Заявка с сайта';          //заголовок и сообщение
+$mail->Body    = '                          
 		Пользователь оставил данные <br> 
-	Имя: ' . $name . ' <br>
-	E-mail: ' . $email . '<br>
-	Сообщение: ' . $text . '';
+        Имя: ' . $name . ' <br>
+        E-mail: ' . $email . '<br>
+        Сообщение: ' . $text . '';
 
-if(!$mail->send()) {
+$sendToDeveloper = $mail->send();       //отправка сообщения
+
+if(!$sendToDeveloper) { //если произошла ошибка, то возвращаем на клиент ошибку
     return false;
-} else {
+} else {                //в случае успешной отправки сообщение разработчику 
+                        //составляем сообщение для клиента и отправляем ему на почту
+    $mail->clearAllRecipients();   //удаляем из объекта адрес разработчика                  
+    $mail->addAddress($email);     //добавляем адресс клиента
+    $mail->Subject = 'Web-разработчик Даниил Катаев';
+    $mail->Body    = '
+            Ваше сообщение успешно отправлено! <br> 
+            Разработчик с Вами свяжется в самое ближайшее время';
+    if(!$mail->send()) {
+        return false;
+    } else {
+        return true;
+    }
     return true;
 }
-
 ?>
+
